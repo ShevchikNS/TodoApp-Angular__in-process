92b416b1f13a10b1263db7642f665468ece8728d
ABC Man <Shevchikkola@gmail.com>
2023-06-29 14:31:13 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {BookmarksComponent} from "./components/bookmarks/bookmarks.component";
+import {TodosComponent} from "./components/todos/todos.component";
+import {NotesComponent} from "./components/notes/notes.component";
+import {AddNoteComponent} from "./components/add-note/add-note.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'bookmarks', component: BookmarksComponent, data: {tab: 1}},
+    {path: 'todos', component: TodosComponent, data: {tab: 2}},
+    {path: 'notes', component: NotesComponent, data: {tab: 3}},
+    {path: 'notes/add', component: AddNoteComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FormsModule} from "@angular/forms";
+import {TabsComponent} from './components/tabs/tabs.component';
+import {BookmarksComponent} from './components/bookmarks/bookmarks.component';
+import {TodosComponent} from './components/todos/todos.component';
+import {NotesComponent} from './components/notes/notes.component';
+import {BookmarkTileComponent} from './components/bookmark-tile/bookmark-tile.component';
+import { AddNoteComponent } from './components/add-note/add-note.component';
+import { NoteCardComponent } from './components/note-card/note-card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TabsComponent,
+        BookmarksComponent,
+        TodosComponent,
+        NotesComponent,
+        BookmarkTileComponent,
+        AddNoteComponent,
+        NoteCardComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        BrowserAnimationsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
